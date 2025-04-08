9ddacbfddfec5dcb54ad292b39cb29015954cd40
andgram <145982786+andgram@users.noreply.github.com>
2025-04-08 08:39:21 +0200

    feat: set up React project with TypeScript #6

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Holidaze</h1>
+      <p>Welcome to your accommodation booking app!</p>
+    </div>
+  );
+}
+
+export default App;

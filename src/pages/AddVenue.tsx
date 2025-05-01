43c63322c88b59c9fb6380169555e649ef7e3df3
andgram <145982786+andgram@users.noreply.github.com>
2025-05-01 20:35:50 +0200

    Add styling to edit venue and create venue pages

--- src/pages/AddVenue.tsx
+++ src/pages/AddVenue.tsx
@@ -58,246 +58,288 @@ function AddVenue() {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-8 bg-white shadow-md rounded-lg">
-      <form onSubmit={handleSubmit}>
-        <h2 className="text-2xl font-semibold text-center mb-6">Add a Venue</h2>
+    <div
+      className="min-h-screen flex items-center justify-center bg-cover bg-center relative"
+      style={{
+        backgroundImage: `url('/create-edit-bg.jpg')`,
+      }}
+    >
+      <div className="max-w-4xl mx-auto p-8 bg-secondary shadow-md rounded-xl my-12">
+        <form onSubmit={handleSubmit}>
+          <h2 className="text-3xl font-semibold text-center text-text mb-10">
+            Create a Venue
+          </h2>
 
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-          <div className="flex flex-col">
-            <label htmlFor="name" className="text-lg font-medium mb-2">
-              Venue Name
-            </label>
-            <input
-              name="name"
-              id="name"
-              placeholder="Venue Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-              className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-
-          <div className="flex flex-col">
-            <label htmlFor="price" className="text-lg font-medium mb-2">
-              Price
-            </label>
-            <input
-              name="price"
-              id="price"
-              placeholder="Price"
-              type="number"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-              min="0"
-              required
-              className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-        </div>
-
-        <div className="flex flex-col mt-6">
-          <label htmlFor="description" className="text-lg font-medium mb-2">
-            Description
-          </label>
-          <textarea
-            name="description"
-            id="description"
-            placeholder="Description"
-            rows={4}
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            required
-            className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 mt-6">
-          <div className="flex flex-col">
-            <label htmlFor="maxGuests" className="text-lg font-medium mb-2">
-              Maximum Guests
-            </label>
-            <input
-              name="maxGuests"
-              id="maxGuests"
-              placeholder="Maximum Guests"
-              type="number"
-              value={maxGuests}
-              onChange={(e) => setMaxGuests(e.target.value)}
-              min="1"
-              required
-              className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-        </div>
-
-        <div className="mt-6">
-          <label className="text-lg font-medium">Media</label>
-          <button
-            type="button"
-            onClick={() => setShowMedia(!showMedia)}
-            className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-          >
-            {showMedia ? "Hide Image" : "Add Image"}
-          </button>
-
-          {showMedia && (
-            <div className="mt-4">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+            <div className="flex flex-col">
+              <label
+                htmlFor="name"
+                className="text-lg font-medium text-text mb-2"
+              >
+                Venue Name
+              </label>
               <input
-                type="url"
-                value={mediaUrl}
-                onChange={(e) => setMediaUrl(e.target.value)}
-                placeholder="https://example.com/image.jpg"
-                className="w-full p-3 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
-              <input
-                type="text"
-                value={mediaAlt}
-                onChange={(e) => setMediaAlt(e.target.value)}
-                placeholder="Image description"
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                name="name"
+                id="name"
+                placeholder="Venue Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                className="border border-secondary p-3 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-accent"
               />
             </div>
-          )}
-        </div>
 
-        <div className="mt-6">
-          <p className="font-medium text-lg">Facilities</p>
-          <div className="space-y-4 mt-4">
-            <div>
-              <label htmlFor="wifi" className="flex items-center">
-                <input
-                  type="checkbox"
-                  name="wifi"
-                  id="wifi"
-                  checked={wifi}
-                  onChange={(e) => setWifi(e.target.checked)}
-                  className="mr-2"
-                />
-                Wifi <span className="text-sm text-gray-600">(Optional)</span>
+            <div className="flex flex-col">
+              <label
+                htmlFor="price"
+                className="text-lg font-medium text-text mb-2"
+              >
+                Price
               </label>
-            </div>
-            <div>
-              <label htmlFor="parking" className="flex items-center">
-                <input
-                  type="checkbox"
-                  name="parking"
-                  id="parking"
-                  checked={parking}
-                  onChange={(e) => setParking(e.target.checked)}
-                  className="mr-2"
-                />
-                Parking{" "}
-                <span className="text-sm text-gray-600">(Optional)</span>
-              </label>
-            </div>
-            <div>
-              <label htmlFor="breakfast" className="flex items-center">
-                <input
-                  type="checkbox"
-                  name="breakfast"
-                  id="breakfast"
-                  checked={breakfast}
-                  onChange={(e) => setBreakfast(e.target.checked)}
-                  className="mr-2"
-                />
-                Breakfast{" "}
-                <span className="text-sm text-gray-600">(Optional)</span>
-              </label>
-            </div>
-            <div>
-              <label htmlFor="pets" className="flex items-center">
-                <input
-                  type="checkbox"
-                  name="pets"
-                  id="pets"
-                  checked={pets}
-                  onChange={(e) => setPets(e.target.checked)}
-                  className="mr-2"
-                />
-                Pets Allowed{" "}
-                <span className="text-sm text-gray-600">(Optional)</span>
-              </label>
+              <input
+                name="price"
+                id="price"
+                placeholder="Price"
+                type="number"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                min="0"
+                required
+                className="border border-secondary p-3 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-accent"
+              />
             </div>
           </div>
-        </div>
 
-        <div className="mt-6">
-          <button
-            type="button"
-            onClick={() => setShowLocation(!showLocation)}
-            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-          >
-            {showLocation
-              ? "Hide Additional Information"
-              : "Add Additional Information"}
-          </button>
+          <div className="flex flex-col mt-6">
+            <label
+              htmlFor="description"
+              className="text-lg font-medium text-text mb-2"
+            >
+              Description
+            </label>
+            <textarea
+              name="description"
+              id="description"
+              placeholder="Description"
+              rows={4}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+              className="border border-secondary p-3 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-accent"
+            />
+          </div>
 
-          {showLocation && (
-            <div className="mt-4">
-              <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-                <div className="flex flex-col">
-                  <label htmlFor="address" className="text-lg font-medium mb-2">
-                    Address
-                  </label>
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 mt-6">
+            <div className="flex flex-col">
+              <label
+                htmlFor="maxGuests"
+                className="text-lg font-medium text-text mb-2"
+              >
+                Maximum Guests
+              </label>
+              <input
+                name="maxGuests"
+                id="maxGuests"
+                placeholder="Maximum Guests"
+                type="number"
+                value={maxGuests}
+                onChange={(e) => setMaxGuests(e.target.value)}
+                min="1"
+                required
+                className="border border-secondary p-3 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-accent"
+              />
+            </div>
+          </div>
+
+          <div className="mt-6">
+            <label className="text-lg font-medium text-text">Media</label>
+            <button
+              type="button"
+              onClick={() => setShowMedia(!showMedia)}
+              className="mt-2 bg-primary text-white px-4 py-1 ml-2 rounded-xl hover:bg-accenthover transition"
+            >
+              {showMedia ? "Remove Image" : "Add Image"}
+            </button>
+
+            {showMedia && (
+              <div className="mt-4">
+                <input
+                  type="url"
+                  value={mediaUrl}
+                  onChange={(e) => setMediaUrl(e.target.value)}
+                  placeholder="https://example.com/image.jpg"
+                  className="w-full p-3 border border-secondary rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-accent mb-4"
+                />
+                <input
+                  type="text"
+                  value={mediaAlt}
+                  onChange={(e) => setMediaAlt(e.target.value)}
+                  placeholder="Image description"
+                  className="w-full p-3 border border-secondary rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-accent"
+                />
+              </div>
+            )}
+          </div>
+
+          <div className="mt-6">
+            <p className="font-medium text-lg text-text">Facilities</p>
+            <div className="space-y-4 mt-4">
+              <div>
+                <label htmlFor="wifi" className="flex items-center">
                   <input
-                    name="address"
-                    id="address"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                    className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    type="checkbox"
+                    name="wifi"
+                    id="wifi"
+                    checked={wifi}
+                    onChange={(e) => setWifi(e.target.checked)}
+                    className="mr-2 accent-accent"
                   />
-                </div>
-                <div className="flex flex-col">
-                  <label htmlFor="city" className="text-lg font-medium mb-2">
-                    City
-                  </label>
+                  Wifi{" "}
+                  <span className="text-sm text-text opacity-60">
+                    (Optional)
+                  </span>
+                </label>
+              </div>
+              <div>
+                <label htmlFor="parking" className="flex items-center">
                   <input
-                    name="city"
-                    id="city"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                    className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    type="checkbox"
+                    name="parking"
+                    id="parking"
+                    checked={parking}
+                    onChange={(e) => setParking(e.target.checked)}
+                    className="mr-2 accent-accent"
                   />
-                </div>
-                <div className="flex flex-col">
-                  <label htmlFor="zip" className="text-lg font-medium mb-2">
-                    Zip
-                  </label>
+                  Parking{" "}
+                  <span className="text-sm text-text opacity-60">
+                    (Optional)
+                  </span>
+                </label>
+              </div>
+              <div>
+                <label htmlFor="breakfast" className="flex items-center">
                   <input
-                    name="zip"
-                    id="zip"
-                    value={zip}
-                    onChange={(e) => setZip(e.target.value)}
-                    className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    type="checkbox"
+                    name="breakfast"
+                    id="breakfast"
+                    checked={breakfast}
+                    onChange={(e) => setBreakfast(e.target.checked)}
+                    className="mr-2 accent-accent"
                   />
-                </div>
-                <div className="flex flex-col">
-                  <label htmlFor="country" className="text-lg font-medium mb-2">
-                    Country
-                  </label>
+                  Breakfast{" "}
+                  <span className="text-sm text-text opacity-60">
+                    (Optional)
+                  </span>
+                </label>
+              </div>
+              <div>
+                <label htmlFor="pets" className="flex items-center">
                   <input
-                    name="country"
-                    id="country"
-                    value={country}
-                    onChange={(e) => setCountry(e.target.value)}
-                    className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    type="checkbox"
+                    name="pets"
+                    id="pets"
+                    checked={pets}
+                    onChange={(e) => setPets(e.target.checked)}
+                    className="mr-2 accent-accent"
                   />
-                </div>
+                  Pets Allowed{" "}
+                  <span className="text-sm text-text opacity-60">
+                    (Optional)
+                  </span>
+                </label>
               </div>
             </div>
-          )}
-        </div>
+          </div>
 
-        <button
-          type="submit"
-          className="mt-6 w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600"
-        >
-          Create Venue
-        </button>
+          <div className="mt-6">
+            <button
+              type="button"
+              onClick={() => setShowLocation(!showLocation)}
+              className="bg-primary text-white px-4 py-1 rounded-xl hover:bg-accenthover transition"
+            >
+              {showLocation
+                ? "Hide Additional Information"
+                : "Add Additional Information"}
+            </button>
 
-        {error && <p className="mt-4 text-red-600 text-center">{error}</p>}
-      </form>
+            {showLocation && (
+              <div className="mt-4">
+                <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                  <div className="flex flex-col">
+                    <label
+                      htmlFor="address"
+                      className="text-lg font-medium text-text mb-2"
+                    >
+                      Address
+                    </label>
+                    <input
+                      name="address"
+                      id="address"
+                      value={address}
+                      onChange={(e) => setAddress(e.target.value)}
+                      className="border border-secondary p-3 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-accent"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label
+                      htmlFor="city"
+                      className="text-lg font-medium text-text mb-2"
+                    >
+                      City
+                    </label>
+                    <input
+                      name="city"
+                      id="city"
+                      value={city}
+                      onChange={(e) => setCity(e.target.value)}
+                      className="border border-secondary p-3 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-accent"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label
+                      htmlFor="zip"
+                      className="text-lg font-medium text-text mb-2"
+                    >
+                      Zip
+                    </label>
+                    <input
+                      name="zip"
+                      id="zip"
+                      value={zip}
+                      onChange={(e) => setZip(e.target.value)}
+                      className="border border-secondary p-3 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-accent"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label
+                      htmlFor="country"
+                      className="text-lg font-medium text-text mb-2"
+                    >
+                      Country
+                    </label>
+                    <input
+                      name="country"
+                      id="country"
+                      value={country}
+                      onChange={(e) => setCountry(e.target.value)}
+                      className="border border-secondary p-3 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-accent"
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+
+          <button
+            type="submit"
+            className="mt-6 w-full bg-primary text-white py-3 rounded-xl font-semibold hover:bg-accenthover transition"
+          >
+            Create Venue
+          </button>
+
+          {error && <p className="mt-4 text-red-500 text-center">{error}</p>}
+        </form>
+      </div>
     </div>
   );
 }
